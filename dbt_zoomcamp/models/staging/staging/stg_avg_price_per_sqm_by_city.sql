c804eedc822af2bc459ba5f2eaa1ec229570828e
Rui Carvalho <ruimcarv@gmail.com>
2025-01-21 20:57:18 +0000

    some models

---
+++ dbt_zoomcamp/models/staging/staging/stg_avg_price_per_sqm_by_city.sql
@@ -0,0 +1,20 @@
+with
+  base as (
+      select
+          city,
+          district,
+          total_area,
+          price,
+          price / nullif(total_area, 0) as price_per_sqm
+      from "mage_db"."public"."real_estate_data"
+      where total_area > 0 and price > 0
+  )
+select
+  city,
+  district,
+  count(*) as property_count,
+  avg(price_per_sqm) as avg_price_per_sqm
+from base
+group by city, district
+order by avg_price_per_sqm desc
+limit 500;
